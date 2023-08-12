1acb74ffceff4f33bc41649b0a94b97a747af181
Muhammad Boborakhimov <muhammadzonboborahimov7@gmail.com>
2023-08-12 12:33:21 +0500

    ADD: fonts-Quicksand; UPDATE: styles, layout;

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,1 +1,8 @@
-<template></template>
+<template>
+  <q-layout class="bg-primary">
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
---
+++ src/pages/Main.vue
@@ -0,0 +1,8 @@
+<template>
+  <q-page>
+    <h1 class="text-h1 text-white">Vue Simple ChatBot</h1>
+  </q-page>
+</template>
+
+<script setup>
+</script>
