854c529d8dfe02acd593adbdce42cc231fcf276a
Muhammad Boborakhimov <muhammadzonboborahimov7@gmail.com>
2023-08-12 16:00:42 +0500

    UPDATE: make responsive styles

--- src/components/ChatWidget.vue
+++ src/components/ChatWidget.vue
@@ -1,7 +1,31 @@
 <template>
-  <q-card class="q-pa-sm chat" style="width: 40%; height: 90vh;">
+  <q-card class="q-pa-sm chat__card" flat>
     <q-card-section>
-      <q-chat-message :text="['Hello', 'World']"></q-chat-message>
+      <q-chat-message avatar="https://cdn.quasar.dev/logo/svg/quasar-logo.svg" :text="messages.bot"></q-chat-message>
+      <q-chat-message avatar="https://cdn.quasar.dev/logo/svg/quasar-logo.svg" :text="messages.user"
+        sent></q-chat-message>
     </q-card-section>
   </q-card>
 </template>
+
+<script setup>
+import { onMounted, ref } from 'vue'
+
+const messages = ref({
+  bot: [],
+  user: []
+})
+
+onMounted(() => {
+  setTimeout(() => {
+    messages.value.bot.push('Hello, how can I help you?')
+  }, 1000)
+})
+</script>
+
+<style>
+.chat__card {
+  height: 50%;
+  width: 100%;
+}
+</style>
--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -1,15 +1,42 @@
 <template>
-  <q-page class="flex q-pa-md no-wrap">
-    <div class="flex text-center column align-center">
-      <h1 class="text-h3 text-white text-bold">Vue Simple ChatBot</h1>
-      <p class="text-h5 text-white">A simple Vue chatbot component</p>
+  <q-page class="flex q-pa-md wrap"
+    :class="{ 'column': $q.screen.xs || $q.screen.sm }, { 'justify-start': $q.screen.sm || $q.screen.xs }, { 'justify-around': $q.screen.md || $q.screen.xl || $q.screen.lg }">
+    <div class="flex text-center column justify-center"
+      :class="{ 'q-mb-lg': $q.screen.xs || $q.screen.sm || $q.screen.md }">
+      <h1 class="text-h3 text-white text-bold"
+        :class="{ 'text-h4': $q.screen.sm || $q.screen.xs }, { 'text-h5': $q.screen.xs }">
+        Vue Simple ChatBot
+      </h1>
+      <p class="text-h5 text-white" :class="{ 'text-h6': $q.screen.md || $q.screen.sm || $q.screen.xs }">A simple Vue
+        chatbot component</p>
     </div>
-    <q-space></q-space>
-    <ChatWidget />
+    <div class="flex items-center chat chat__container" :class="{ 'q-mx-auto': $q.screen.xs || $q.screen.sm }">
+      <ChatWidget />
+    </div>
   </q-page>
 </template>
 
 <script setup>
 import ChatWidget from 'src/components/ChatWidget.vue';
-
 </script>
+
+<style lang="scss">
+.chat {
+  &__container {
+    width: 30%;
+    min-width: 20%;
+  }
+}
+
+@media (max-width: 1023.99px) {
+  .chat__container {
+    width: 70%;
+  }
+}
+
+@media (max-width: 599.99px) {
+  .chat__container {
+    width: 100%;
+  }
+}
+</style>
