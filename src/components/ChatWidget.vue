d4f003b1d5aeaf28a5c978e7d1df94b6c4c2bc9e
Muhammad Boborakhimov <muhammadzonboborahimov7@gmail.com>
2023-08-12 12:57:09 +0500

    ADD: chat base model

---
+++ src/components/ChatWidget.vue
@@ -0,0 +1,7 @@
+<template>
+  <q-card class="q-pa-sm chat" style="width: 40%; height: 90vh;">
+    <q-card-section>
+      <q-chat-message :text="['Hello', 'World']"></q-chat-message>
+    </q-card-section>
+  </q-card>
+</template>
--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -1,8 +1,15 @@
 <template>
-  <q-page>
-    <h1 class="text-h1 text-white">Vue Simple ChatBot</h1>
+  <q-page class="flex q-pa-md no-wrap">
+    <div class="flex text-center column align-center">
+      <h1 class="text-h3 text-white text-bold">Vue Simple ChatBot</h1>
+      <p class="text-h5 text-white">A simple Vue chatbot component</p>
+    </div>
+    <q-space></q-space>
+    <ChatWidget />
   </q-page>
 </template>
 
 <script setup>
+import ChatWidget from 'src/components/ChatWidget.vue';
+
 </script>
